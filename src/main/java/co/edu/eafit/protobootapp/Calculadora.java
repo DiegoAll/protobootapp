03853ff3ccbbb44a51e9c4a31c041c2bc57b40db
DiegoAll <dposadallano@gmail.com>
2023-03-11 09:18:39 -0500

    test comment by Diego

--- src/main/java/co/edu/eafit/protobootapp/Calculadora.java
+++ src/main/java/co/edu/eafit/protobootapp/Calculadora.java
@@ -1,6 +1,6 @@
 package co.edu.eafit.protobootapp;
 
-//This is a simple calculator class.
+//This is a simple calculator class. test comment by Diego
 public class Calculadora {
 
     public Double cuadrado(Double numero) {
